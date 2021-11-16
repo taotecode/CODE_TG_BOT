fbe5de6cdcb9b11b537d515d7d814a2d03c82520
yuanzhumc <yuanzhumc@163.com>
2021-11-16 13:54:43 +0800

    数据加入重复检测

--- app/admin/controller/code/TgCommand.php
+++ app/admin/controller/code/TgCommand.php
@@ -70,4 +70,51 @@ class TgCommand extends AdminController
         $this->assign('commandAdd',trim($commandAdd,'<br>'));
         return $this->fetch();
     }
+
+    /**
+     * @NodeAnotation(title="添加")
+     */
+    public function add()
+    {
+        if ($this->request->isPost()) {
+            $post = $this->request->post();
+            $rule = [];
+            $this->validate($post, $rule);
+            if ($this->model->where('command',$post['command'])->find()){
+                $this->error('该命令已存在！');
+            }
+            try {
+                $save = $this->model->save($post);
+            } catch (\Exception $e) {
+                $this->error('保存失败:'.$e->getMessage());
+            }
+            $save ? $this->success('保存成功') : $this->error('保存失败');
+        }
+        return $this->fetch();
+    }
+
+    /**
+     * @NodeAnotation(title="编辑")
+     */
+    public function edit($id)
+    {
+        $row = $this->model->find($id);
+        empty($row) && $this->error('数据不存在');
+        if ($this->request->isPost()) {
+            $post = $this->request->post();
+            $rule = [];
+            $this->validate($post, $rule);
+            if ($this->model->where('command',$post['command'])->whereNotIn('id',[$id])->find()){
+                $this->error('该命令已存在！');
+            }
+            try {
+                $save = $row->save($post);
+            } catch (\Exception $e) {
+                $this->error('保存失败');
+            }
+            $save ? $this->success('保存成功') : $this->error('保存失败');
+        }
+        $this->assign('row', $row);
+        return $this->fetch();
+    }
 }
